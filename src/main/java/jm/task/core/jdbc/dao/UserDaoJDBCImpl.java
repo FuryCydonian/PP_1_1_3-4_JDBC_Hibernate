a85d2ca669c06a0145f933ffbef2d2b1f3faa8f3
fury_cydonian <fury_cydonian@pop-os.localdomain>
2022-03-22 01:45:11 +0300

    add connection logic in Util class, add statement in createUserTable()

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -9,8 +14,22 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-    public void createUsersTable() {
+    Connection connection = Util.getConnection();
 
+    public void createUsersTable() {
+        try {
+            Statement statemet = connection.createStatement();
+            String SQL = "CREATE TABLE Users (" +
+                    "id int" +
+                    "name varchar" +
+                    "lastName varchar" +
+                    "age int" +
+                    ")";
+            ResultSet resultSet = statemet.executeQuery(SQL);
+
+        } catch (SQLException e) {
+            System.out.println("Table is already exist");
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/test_PP-bir";
+    private static final String USERNAME = "fury_cydonian";
+    private static final String PASSWORD = "ThomasBorchert10!";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
