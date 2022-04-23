63cb0ca544a7a955d77f3e1e990ac754627cc330
Ella Rudolstadt <36266468+FuryCydonian@users.noreply.github.com>
2022-04-23 18:56:48 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -14,5 +14,7 @@ public class Main {
         System.out.println(service.getAllUsers());
         service.cleanUsersTable();
         service.dropUsersTable();
+        
+        Util.closeSessionFactory();
     }
 }
